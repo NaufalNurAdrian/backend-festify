9285a4d6645a764037a93181bc67dc8a4564f7ce
hashfimw <hashfimawarid00@gmail.com>
2024-12-12 15:19:26 +0700

    add eventcontoller

--- backend/src/controllers/event.controller.ts
+++ backend/src/controllers/event.controller.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
 import prisma from "../prisma";
+import { Prisma } from "@prisma/client";
 
 export class EventController {
   async getEventId(req: Request, res: Response) {
     try {
+      const { search } = req.query;
+      const filter: Prisma.EventWhereInput = {};
+      if (search) {
+        filter.title = { contains: search as string, mode: "insensitive" };
+      }
       const events = await prisma.event.findMany({
+        where: filter,
         select: {
           event_id: true,
           title: true,
@@ -65,6 +72,7 @@ export class EventController {
           },
         },
       });
+      res.status(200).send({ events });
     } catch (err) {
       console.log(err);
       res.status(400).send({ message: "Error Not Found" });
