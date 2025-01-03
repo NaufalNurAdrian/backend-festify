b768de14011fb445247393dc512d353e101bf977
hashfimw <hashfimawarid00@gmail.com>
2025-01-04 05:42:13 +0700

    edit transaction payment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { AuthRouter } from "./routers/auth.router";
-// import path from "path";
+import path from "path";
 import cookieParser from "cookie-parser";
 import { UserRouter } from "./routers/user.router";
 import { EventRouter } from "./routers/event.router";
@@ -9,7 +9,7 @@ import { EventRouter } from "./routers/event.router";
 import { TicketRouter } from "./routers/ticket.router";
 
 import { DashboardRouter } from "./routers/dashboard.router";
-// import { TransactionRouter } from "./routers/order.router";
+import { TransactionRouter } from "./routers/order.router";
 
 const PORT: number = 8000;
 
@@ -32,14 +32,14 @@ const authRouter = new AuthRouter();
 const userRouter = new UserRouter();
 const eventsRouter = new EventRouter();
 const ticketRouter = new TicketRouter();
-// const transactionRouter = new TransactionRouter();
+const transactionRouter = new TransactionRouter();
 const dashboardRouter = new DashboardRouter();
 
 app.use("/api/auth", authRouter.getRouter());
 app.use("/api/users", userRouter.getRouter());
 app.use("/api/event", eventsRouter.getRouter());
 app.use("/api/tickets", ticketRouter.getRouter());
-// app.use("/api/transactions", transactionRouter.getRouter());
+app.use("/api/transactions", transactionRouter.getRouter());
 app.use("/api/dashboard", dashboardRouter.getRouter());
 
 app.listen(PORT, () => {
